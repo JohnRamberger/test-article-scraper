aac0e8f1eaa56927ad2874f1674693fb79af8cd1
JohnRamberger <jramberger3@gatech.edu>
2022-11-28 22:10:43 -0500

    Update SourceManager.class.ts

--- src/SourceManager.class.ts
+++ src/SourceManager.class.ts
@@ -1,7 +1,13 @@
 import { Queue } from "./Queue.class";
 
 export class SourceManager {
-  static queue: Queue;
+  /**
+   * The queue of sources to be processed.
+   */
+  static queue: Queue<string>;
+  /**
+   * Initializes the queue.
+   */
   static init() {
     this.queue = new Queue();
   }
